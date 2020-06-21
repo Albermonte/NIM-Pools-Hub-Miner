56c025f08eee67c1963c06386dc8107be693b46f
Albermonte <alberto.monterroso@hotmail.com>
2020-06-21 16:38:09 +0200

    Nimiq style finished, added Hashrate Chart

---
+++ src/renderer/components/LineChart.js
@@ -0,0 +1,23 @@
+import { Line, mixins } from 'vue-chartjs'
+const { reactiveProp } = mixins
+
+export default {
+    extends: Line,
+    mixins: [reactiveProp],
+    props: ['options'],
+    mounted() {
+        // this.chartData is created in the mixin.
+        // If you want to pass options please create a local options object
+        this.renderChart(this.chartData, {
+            legend: {
+                display: false
+            },
+            tooltips: {
+                enabled: true
+            },
+            responsive: true,
+            maintainAspectRatio: false,
+            
+        })
+    }
+}
