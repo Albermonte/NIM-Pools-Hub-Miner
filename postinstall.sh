3a934a7ad396d04a8c2c2e5dba79ebef9b6cd617
Alberto <albermonte@Albertos-Mac.local>
2020-06-30 03:01:29 -0700

    No need for special bindings on Linux and Mac for node-lmdb

--- postinstall.sh
+++ postinstall.sh
@@ -1,6 +1,4 @@
 cp "./bindings/SushiPool_binding.gyp" "./node_modules/@nimiq/core/binding.gyp"
-cp "./bindings/Albermonte_binding.gyp" "./node_modules/node-lmdb/binding.gyp"
-cp "./bindings/Albermonte_binding.gyp" "./node_modules/@nimiq/jungle-db/node_modules/node-lmdb/binding.gyp"
 
 npm rebuild
 
