4e70656542e06b99eb49c1cc790a5a1f02021ba8
Albermonte <alberto.monterroso@hotmail.com>
2020-07-26 12:55:09 +0200

    Rename Landing to CPU

---
+++ src/renderer/pages/CPUPage.vue
@@ -0,0 +1,318 @@
+<template>
+  <div class="container">
+    <div class="row py-2">
+      <div v-if="development" class="col">
+        <input
+          placeholder="Nimiq Address"
+          id="address"
+          v-model="address"
+          class="nq-input-s text-center"
+          style="width: 100%; text-transform:uppercase;"
+        />
+      </div>
+      <div v-else class="col">
+        <input
+          placeholder="Nimiq Address"
+          v-model="address"
+          class="nq-input-s text-center"
+          style="width: 100%; text-transform:uppercase;"
+        />
+      </div>
+    </div>
+    <div class="row space-between py-2">
+      <BalanceCard />
+      <HashrateCard />
+    </div>
+    <div class="row py-2">
+      <line-chart :chart-data="datacollection" style="height: 120px; width:100%"></line-chart>
+    </div>
+    <div class="row">
+      <div class="col space-between">
+        <div class="row" style="width: 100%; align-items: center; margin-top:5px">
+          <h1 class="nq-h1">Pool: {{ displayName }}</h1>
+        </div>
+        <button
+          class="nq-button orange"
+          @click="selectPool"
+          :class="mining ? 'disabled' : ''"
+        >Change Pool</button>
+      </div>
+      <div class="col space-between">
+        <RangeSlider :class="mining ? 'disabled' : ''" />
+        <button v-if="!mining" @click="startMining" class="nq-button light-blue">Start Mining</button>
+        <button v-else @click="stopMining" class="nq-button red">Stop Mining</button>
+      </div>
+    </div>
+    <Alert :message="alertMessage" v-if="showAlert" @hideAlert="hideAlertHandler" />
+    <span class="app-version">v: {{ appVersion }}</span>
+    <!-- Disabled until Temperatures are reliable, getting 16ºC on my PC, thanks to wmic reporting bad temperatures -->
+    <!-- <span class="cpu-temp">CPU: {{ cpuTemp }} ºC</span> -->
+  </div>
+</template>
+
+<script>
+import { ipcRenderer } from "electron";
+import HashrateCard from "@/components/HashrateCard.vue";
+import BalanceCard from "@/components/BalanceCard.vue";
+import LineChart from "@/components/LineChart.js";
+import RangeSlider from "@/components/RangeSlider.vue";
+import Alert from "@/components/Alert.vue";
+
+import * as NimiqUtils from "@nimiq/utils";
+
+const Store = require("electron-store");
+const store = new Store();
+
+export default {
+  name: "cpu-page",
+  components: {
+    HashrateCard,
+    BalanceCard,
+    LineChart,
+    RangeSlider,
+    Alert,
+  },
+  data() {
+    return {
+      address:
+        process.env.NODE_ENV === "development"
+          ? "NQ65 GS91 H8CS QFAN 1EVS UK3G X7PL L9N1 X4KC"
+          : store.get("address"),
+      host: store.get("host") || "eu.nimpool.io",
+      port: store.get("port") || 8444,
+      displayName: store.get("poolDisplayName") || "Nimpool",
+      mining: false,
+      datacollection: {
+        labels: null,
+        datasets: [
+          {
+            label: "Hashrate",
+            borderColor: "#E9B213",
+            pointBorderColor: "#E9B213",
+            pointBackgroundColor: "#E9B213",
+            pointBorderWidth: 5,
+            pointRadius: 2,
+            fill: false,
+            borderWidth: 3,
+            data: null,
+          },
+        ],
+      },
+      time: [],
+      hashrate: [],
+      alertMessage: null,
+      showAlert: false,
+      appVersion: "0.0.0",
+      cpuTemp: 0,
+    };
+  },
+  mounted() {
+    store.set("page", "cpu");
+
+    ipcRenderer.on("hashrate-update", (event, message) => {
+      this.hashrate.push(Number(message.split(" ")[0]));
+      if (this.hashrate.length > 15) {
+        this.hashrate.shift();
+      }
+
+      this.time.push(
+        `${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`
+      );
+      if (this.time.length > 15) {
+        this.time.shift();
+      }
+
+      this.datacollection = {
+        labels: this.time,
+        datasets: [
+          {
+            label: "Hashrate",
+            borderColor: "#E9B213",
+            pointBorderColor: "#E9B213",
+            pointBackgroundColor: "#E9B213",
+            pointBorderWidth: 5,
+            pointRadius: 2,
+            fill: false,
+            borderWidth: 3,
+            data: this.hashrate,
+          },
+        ],
+      };
+    });
+
+    // If updated finished downloading Alert the user the app is going to restart
+    ipcRenderer.on("update-downloaded", () => {
+      this.alert("New update downloaded, restarting in 5 seconds...");
+    });
+
+    // Ask backend which is the app version
+    ipcRenderer.send("app-version");
+    ipcRenderer.on("app-version-reply", (event, message) => {
+      console.log("App Version: " + message);
+      this.appVersion = message;
+    });
+
+    // Disabled until Temperatures are reliable, getting 16ºC on my PC, thanks to wmic reporting bad temperatures
+    // Check immediately what's the CPU Temp and add an interval
+    /* ipcRenderer.send("cpu-temp");
+    setInterval(() => ipcRenderer.send("cpu-temp"), 1 * 60 * 1e3); // Check CPU temperature every x minutes
+    ipcRenderer.on("cpu-temp-reply", (event, message) => {
+      this.cpuTemp =
+        message > 0 ? message : "Run this app as Admin to check CPU ";
+    }); */
+
+    // If backend detects this device as laptop and not chargin tell the user to plug it
+    ipcRenderer.on("laptopNotChargin", () => {
+      this.alert(
+        "You seem to be mining on a Laptop but it's not charging, maybe you want to plug it in ;D"
+      );
+    });
+  },
+  computed: {
+    development() {
+      return process.env.NODE_ENV === "development";
+    },
+  },
+  methods: {
+    startMining() {
+      ipcRenderer.send("stopMining");
+      console.log(this.address);
+      store.set("address", this.address);
+
+      const address = this.address;
+      const host = this.host;
+      const port = this.port;
+      console.log({
+        address,
+        host,
+        port,
+      });
+      if (address === "" || host === "" || port === "") {
+        this.alert("Fill all the inputs: Address, Pool Host and Pool Port");
+        return;
+      } else {
+        try {
+          NimiqUtils.ValidationUtils.isUserFriendlyAddress(address);
+        } catch (e) {
+          this.alert(e);
+          return;
+        }
+        this.mining = true;
+        //const expresion = /(vue-component-\d+-balance-card)/i
+        //console.log(this.$children);
+        const hashrateComponent = this.$children.find((x) =>
+          x.$vnode.tag.includes("hashrate-card")
+        );
+        hashrateComponent.startMining();
+
+        const balanceComponent = this.$children.find((x) =>
+          x.$vnode.tag.includes("balance-card")
+        );
+        balanceComponent.startMining();
+
+        ipcRenderer.send("startMining", {
+          address,
+          host,
+          port,
+        });
+      }
+    },
+    stopMining() {
+      this.mining = false;
+      ipcRenderer.send("stopMining");
+      const hashrateComponent = this.$children.find((x) =>
+        x.$vnode.tag.includes("hashrate-card")
+      );
+      hashrateComponent.stopMining();
+    },
+    alert(e) {
+      this.alertMessage = e;
+      this.showAlert = true;
+    },
+    hideAlertHandler() {
+      this.showAlert = false;
+      this.alertMessage = null;
+    },
+    selectPool() {
+      /* this.$emit("selectPool"); */
+      this.$router.replace("pools");
+    },
+  },
+};
+</script>
+
+<style>
+.container {
+  width: calc(630px - 100px);
+  height: calc(100% - 47px);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.row {
+  display: flex;
+  width: 80vw;
+  justify-content: space-around;
+}
+
+.col {
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.space-between {
+  justify-content: space-between !important;
+}
+
+.py-2 {
+  padding-top: 8px;
+  padding-bottom: 8px;
+}
+
+.py-3 {
+  padding-top: 14px;
+  padding-bottom: 14px;
+}
+
+.text-center {
+  text-align: center;
+}
+
+.app-version {
+  position: absolute;
+  bottom: 0;
+  right: 5px;
+  font-size: 12px;
+}
+
+.cpu-temp {
+  position: absolute;
+  bottom: 0;
+  left: 5px;
+  font-size: 12px;
+}
+
+.nq-button {
+  margin: 1.5rem !important;
+}
+
+.nq-h1 {
+  margin: 0 !important;
+}
+
+.nq-h2 {
+  margin: 0 !important;
+}
+
+.disabled {
+  pointer-events: none;
+  opacity: 0.65;
+}
+</style>
