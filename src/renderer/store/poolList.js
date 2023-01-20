88eb5176a5e294ba2034e9087361e5689f3eb9fa
Albermonte <14013679+Albermonte@users.noreply.github.com>
2023-01-20 16:58:05 +0100

    Removed not working pools

--- src/renderer/store/poolList.js
+++ src/renderer/store/poolList.js
@@ -4,30 +4,15 @@ const poolListArray = [
     displayName: "AceMining",
     url: "pool.acemining.co:8443",
   },
-  /* {
-     name: "nimpool",
-     displayName: "Nimpool",
-     url: "eu.nimpool.io:8444",
-   }, */
   {
-    name: "blankpool",
-    displayName: "BlankPool",
-    url: "mine.blank.drawpad.org:8444",
+    name: "nimiqwatch",
+    displayName: "Nimiq.Watch",
+    url: "pool.nimiq.watch:8443",
   },
   {
     name: "icemining",
     displayName: "Icemining",
     url: "nimiq.icemining.ca:2053",
-  },
-  {
-    name: "skypool",
-    displayName: "Skypool",
-    url: "sh1.nimiq.skypool.xyz:5000",
-  },
-  {
-    name: "nimiqwatch",
-    displayName: "Nimiq.Watch",
-    url: "pool.nimiq.watch:8443",
   },
 ];
 
